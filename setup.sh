ee09defcb35f0926d5334e3eba58c0882a2a4601
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2017-03-09 22:53:19 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 THIS=$(cd $(dirname $0);pwd)
-DOTFILES=(.zshenv .zshrc .zshrc.antigen .gitconfig .gitignore .tmux.conf)
+DOTFILES=(.zshenv .zshrc .zshrc.antigen .zshrc.alias .gitconfig .gitignore .tmux.conf)
 
 for file in ${DOTFILES[@]}
 do
